f0f57d50c6750df034da4f51e49ce0894e8d3846
Ian Manlupig <imanlupig@academic.rrc.ca>
2025-01-10 13:43:58 -0600

    finished sandbox_while.js, created sandbox_if.js

---
+++ chapter_3/sandbox_while.js
@@ -0,0 +1,23 @@
+// while loops
+const names = ['ian', 'jaden', 'marc'];
+// let i = 0;
+
+// while(i < 5){
+//   console.log('in loop:', i);
+//   i++;
+// }
+// console.log('loop completed');
+
+// let i = 0;
+// while(i < names.length){
+//   console.log(names[i]);
+//   i++;
+// }
+
+// do while loops
+let i = 0;
+do{
+  console.log(names[i]);
+  i++;
+}
+while (i > names.length);
