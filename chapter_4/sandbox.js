4a77874f735322c1fa42f7072e85a6a617a65103
Ian Manlupig <imanlupig@academic.rrc.ca>
2025-01-12 14:22:20 -0600

    chapter4 branch for working with functions

---
+++ chapter_4/sandbox.js
@@ -0,0 +1,21 @@
+// function declarations
+// function greet(){
+//   console.log('Hello there!');
+// }
+
+// // function expression
+// const speak = function(){
+//   console.log('Good Day!');
+// };
+
+// greet();
+// speak();
+
+// arguments and parameters
+
+const speak = function(name = 'Mario', time = 'Night'){
+  console.log(`Good ${time} ${name}`);
+};
+
+speak('ian', 'Morning');
+speak();
