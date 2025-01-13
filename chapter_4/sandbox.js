e850dd587bad0601d6d96278b3f0dca1b66995cb
Ian Manlupig <imanlupig@academic.rrc.ca>
2025-01-13 14:53:59 -0600

    work with callback functions and forEach method

--- chapter_4/sandbox.js
+++ chapter_4/sandbox.js
@@ -31,21 +31,50 @@
 
 
 // arrow function
-let calcAreaArrow = radius => 3.14 * radius**2;
-console.log(calcAreaArrow(5));
+// let calcAreaArrow = radius => 3.14 * radius**2;
+// console.log(calcAreaArrow(5));
 
-// practice arrow function
-let greet = () => 'Hello World';
-console.log(greet());
+// // practice arrow function
+// let greet = () => 'Hello World';
+// console.log(greet());
 
-// #2
-let bill = (products, tax) => {
-  let total = 0;
-  for(let i = 0; i < products.length; i++){
-    total += products[i] + products[i] * tax;
-  }
-  return total;
-}
+// // #2
+// let bill = (products, tax) => {
+//   let total = 0;
+//   for(let i = 0; i < products.length; i++){
+//     total += products[i] + products[i] * tax;
+//   }
+//   return total;
+// }
 
-let newBill = bill([10, 15, 30], .2);
-console.log(newBill);
+// let newBill = bill([10, 15, 30], .2);
+// console.log(newBill);
+
+// const name = 'ian';
+// // functions
+// const greet = () => 'hello'
+// let resultOne = greet();
+// console.log(resultOne);
+
+// // methods
+// let resultTwo = name.toUpperCase();
+// console.log(resultTwo);
+
+
+// callbacks & foreach
+
+const myFunc = (callbackFunc) => {
+  let value = 50;
+  callbackFunc(value);
+};
+
+myFunc(function(value){
+  console.log(value);
+});
+
+let people = ['mario', 'luigi', 'ryu', 'peach', 'shen'];
+
+const logPerson = (person, index) => console.log(`${index} - Hello ${person}`);
+people.forEach(logPerson);
+
+// people.forEach((person,index) => console.log('position: ' + index, person));
