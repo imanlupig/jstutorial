1f099671c18ffb2fb5898087ccc8b51bea3ebf29
Ian Manlupig <imanlupig@academic.rrc.ca>
2025-01-13 09:46:00 -0600

    work with returning values and arrow functions

--- chapter_4/sandbox.js
+++ chapter_4/sandbox.js
@@ -22,3 +22,30 @@
 
 // returning values
 
+// const calcArea = function(radius){
+//   return 3.14 * radius**2;
+// }
+
+// let area = calcArea(5);
+// console.log(area);
+
+
+// arrow function
+let calcAreaArrow = radius => 3.14 * radius**2;
+console.log(calcAreaArrow(5));
+
+// practice arrow function
+let greet = () => 'Hello World';
+console.log(greet());
+
+// #2
+let bill = (products, tax) => {
+  let total = 0;
+  for(let i = 0; i < products.length; i++){
+    total += products[i] + products[i] * tax;
+  }
+  return total;
+}
+
+let newBill = bill([10, 15, 30], .2);
+console.log(newBill);
