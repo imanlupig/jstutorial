322dd23f4d60c9ce3fbd92f84a20475dbea84271
Ian Manlupig <imanlupig@academic.rrc.ca>
2025-01-14 08:54:49 -0600

    worked with different element selectors

--- chapter_6/sandbox.js
+++ chapter_6/sandbox.js
@@ -10,9 +10,21 @@
 // const header1 = document.querySelector('body > h1');
 // console.log(header1);
 
-const paras = document.querySelectorAll('p');
-paras.forEach(para => console.log(para))
+// const paras = document.querySelectorAll('p');
+// paras.forEach(para => console.log(para))
 
-const errors = document.querySelectorAll('.error');
-console.log(errors);
+// const errors = document.querySelectorAll('.error');
+// console.log(errors);
 
+// // get an element by ID
+// const title = document.getElementById('page-title');
+// console.log(title);
+
+// // get elements by their class name
+// const errors = document.getElementsByClassName('error');
+// console.log(errors)
+
+// get elements by tag name
+const paras = document.getElementsByTagName('p');
+console.log(paras);
+console.log(paras[0]);
