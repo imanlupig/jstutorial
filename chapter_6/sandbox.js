89416186bf8834f415e387b70e82b82dad3b957a
Ian Manlupig <imanlupig@academic.rrc.ca>
2025-01-14 08:42:48 -0600

    initial commit for chapter 6, working with DOM

---
+++ chapter_6/sandbox.js
@@ -0,0 +1,18 @@
+// const para = document.querySelector('p');
+// console.log(para)
+
+// const error = document.querySelector('.error')
+// console.log(error)
+
+// const error2 = document.querySelector('div.error');
+// console.log(error2);
+
+// const header1 = document.querySelector('body > h1');
+// console.log(header1);
+
+const paras = document.querySelectorAll('p');
+paras.forEach(para => console.log(para))
+
+const errors = document.querySelectorAll('.error');
+console.log(errors);
+
